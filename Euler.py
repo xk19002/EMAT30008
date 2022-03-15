85897e02ae88ce9fd23accf45687ffe5161fcde8
Tefkros Chimonas <xk19002@bristol.ac.uk>
2022-03-15 18:33:43 +0000

    w3.1.1 created euler_step function

---
+++ Euler.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def euler_step(f,tspan,x0,deltat_max):
+    '''
+    Euler method function to solve x'=f(x,t) 
+    Parameters:
+    f     = function of two variables f = f(t,x) that defines the
+            right-hand side of the ODE to be solved: f' = f(t,x)
+    tspan = the time over which to integrate
+    x0    = initial condtion vector x(t0) = x0
+    N     = number of steps to split integration time into
+
+    '''
+
+    t = np.zeros(deltat_max + 1)
+    x = np.zeros(deltat_max + 1)
+
+    x[0] = x0
+    t[0] = 0
+    h = tspan/deltat_max
+    for k in range(deltat_max):
+        t[k + 1] = t[k] + h
+        x[k + 1] = x[k] + h * f(x[k], t[k])
+    return x,t
+
+
+
+        
+
+
