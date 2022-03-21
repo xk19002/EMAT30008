2f081e4370851fcbfb0f45e7b529a18d3c4235d6
Tefkros Chimonas <xk19002@bristol.ac.uk>
2022-03-21 12:14:02 +0000

    implementing euler and RK4 schemes on new ode solver file

--- euler_rk4_solver.py
+++ euler_rk4_solver.py
@@ -29,7 +29,24 @@ class ode_solver:
     def next_iter(self):
         raise NotImplementedError
 
+class euler_scheme(ode_solver):
+    def next_iter(self):
+        x,f,k,t = self.x, self.f, self.k, self.t
+        deltat_max = t[k+1] - t[k]
+        return x[k,:] + deltat_max * f(x[k,:], t[k])
 
+class RK4_scheme(ode_solver):
+    def next_iter(self):
+        x,f,k,t = self.x, self.f, self.k, self.t
+        deltat_max = t[k + 1] - t[k]
+        half_deltat_max = deltat_max/2
+        sol1 = deltat_max * f(x[k,:], t[k])
+        sol2 = deltat_max *f(x[k,:] + 0.5 * sol1, t[k] + half_deltat_max)
+        sol3 = deltat_max *f(x[k,:] + 0.5 * sol2, t[k] + half_deltat_max)
+        sol4 = deltat_max *f(x[k,:] + sol3, t[k] + half_deltat_max)
+        return x[k,:] + (1/6) * (sol1 + 2 * sol2 + 2 * sol3 + sol4)
+
+        
         
 
 
