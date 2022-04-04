33c76f0d815f5e6ac57eecf1b84a522936765795
Tefkros Chimonas <xk19002@bristol.ac.uk>
2022-04-04 18:40:33 +0100

    fixed type error

--- numerical_shooting.py
+++ numerical_shooting.py
@@ -24,9 +24,9 @@ u2[0] = 0
 analytic_sol1[0] = 0
 analytic_sol2[0] = 0
 
-for i in range (1,N):
-    w1[i] = w1[i-1] + h * (b*u1[i-1] - u2[i-1] + sigma*u1[i-1]*((u1[i-1])^2 + (u2[i-1])^2))
-    w2[i] = w2[i-1] + h * (u1[i-1] + b*u2[i-1] + sigma*u2[i-1]*((u1[i-1])^2 + (u2[i-1])^2)) 
+for i in range (N):
+    w1[i] = w1[i-1] + h * (b*u1[i-1] - u2[i-1] + sigma*u1[i-1]*((u1[i-1])**2 + (u2[i-1])**2))
+    w2[i] = w2[i-1] + h * (u1[i-1] + b*u2[i-1] + sigma*u2[i-1]*((u1[i-1])**2 + (u2[i-1])**2)) 
     u1[i] = u1[i-1] + h
     u2[i] = u2[i-1] + h
     analytic_sol1[i] = np.sqrt(b) * math.cos(i + theta)
