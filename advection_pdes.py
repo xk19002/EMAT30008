dd0a3209b525144c1b803956daa4e306947fc46b
Tefkros Chimonas <xk19002@bristol.ac.uk>
2022-04-22 14:53:59 +0100

    advection with forward euler

---
+++ advection_pdes.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from schemes import euler_scheme
+
+def forward_wave_rhs(x,xdiff,v):
+    gridx = x.shape[0]
+    func = np.empty(gridx)
+    func[1:-1] = -v*(x[2:]-x[1:-1])/xdiff
+    func[0] = 0
+    func[-1] = 0
+    return func
+
+v = 1
+ld = 1
+tend = 0.20
+tstep = 0.005
+ntstep = int(tend/tstep)
+gridx = 101
+xdiff = ld/(gridx-1)
+xc = np.linspace(0,ld,gridx)
+x0 = np.exp(-200*(xc-0.25)**2)
+x = np.empty((ntstep+1,gridx))
+x[0] = x0.copy()
+
+for k in range(ntstep):
+    x[k+1] = euler_scheme(x[k], forward_wave_rhs,tstep,xdiff,v)
+
+fig,ax = plt.subplots(figsize=(10,5))
+
+ax.plot(xc, x[0], label='Initial condition')
+ax.plot(xc,x[int(0.10/tstep)],color='purple',label='t=0.10')
+ax.plot(xc,x[int(0.15/tstep)],color='red',label='t=0.15')
+ax.plot(xc,x[int(tend/tstep)],color='green',label=f't={tend}')
+ax.set_xlabel('$x$')
+ax.set_ylabel('$u$')
+ax.set_title('Advection solution using forward Euler scheme and forward finite differences')
+ax.legend()
+plt.show()
+
+
